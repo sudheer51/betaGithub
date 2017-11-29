d4227c7bc1ccc04ee7a4afb5cd6b101c9c3deeab
radpuri <radpuri@10.211.55.3>
2017-11-29 13:06:38 -0800

    local to global

---
+++ mmppatient/src/main/java/com/mmp/patientmodule/pages/adminLoginPage.java
@@ -0,0 +1,12 @@
+package com.mmp.patientmodule.pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class adminLoginPage {
+
+	WebDriver driver;
+	public adminLoginPage(WebDriver driver) {
+		this.driver=driver;
+	}
+
+}
